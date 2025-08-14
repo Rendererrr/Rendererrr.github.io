c2f8bfa278b1d7a9516d1c3f28f98a94d292de90
Debugger <30207948+Sys0xdbg@users.noreply.github.com>
2025-08-14 10:38:21 -0400

    oh no black

--- test.py
+++ test.py
@@ -4,7 +4,7 @@ def to_uc_vector(hex_string: str) -> list[int]:
     return list(raw_bytes) + [0]  # add null terminator like in C++
 
 if __name__ == "__main__":
-    input_value = "796f7572206e69676765"  # this is "your nigge" in hex
+    input_value = "61726520796f75"  # this is "your nigge" in hex
     uc_vector = to_uc_vector(input_value)
 
     with open("output.txt", "wb") as f:
